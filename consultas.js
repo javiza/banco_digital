6284770885cc9baf35ed3a100695057a283e3d91
Jonathan Bustos <jonathanbust@gmail.com>
2022-06-03 02:26:21 -0400

    mostrar transferencias falta mostrar solo la trasnferencia Hecha por el usuario

--- consultas.js
+++ consultas.js
@@ -1,36 +1,92 @@
-
 const pg = require('pg');
 const pool = new pg.Pool({
-    user: "jona",
-    password: '1234',
-    host: 'localhost',
-    port: 5432,
-    database: 'banco_digital',
+  user: "jona",
+  password: '1234',
+  host: 'localhost',
+  port: 5432,
+  database: 'banco_bit',
 });
 async function register_user(user_data) {
-    const consultas = {
-      text: 'INSERT INTO cliente (name, email, rut, address, password) VALUES ($1, $2, $3, $4, $5)',
-      values: Object.values(user_data),
-    };
-    try {
-      await pool.query(consultas);
-    } catch (error) {
-      console.log(error.message);
-    }
+  const consultas = {
+    text: 'INSERT INTO cliente (name, email, rut, address, password) VALUES ($1, $2, $3, $4, $5)',
+    values: Object.values(user_data),
+  };
+  try {
+    await pool.query(consultas);
+  } catch (error) {
+    console.log(error.message);
   }
-  
+}
+
 async function login(user_data) {
-    const consultas = { 
-        text: 'SELECT * FROM cliente WHERE rut = $1 and password = $2',
-        values: Object.values(user_data)
-    };
-    try {
-        const resp = await pool.query(consultas);
-        return resp;
-        
-    } catch (error) {
-        console.log(error.message);
-      }
-    }
+  const consultas = {
+    text: 'SELECT * FROM cliente WHERE rut = $1 and password = $2',
+    values: Object.values(user_data)
+  };
+  try {
+    const resp = await pool.query(consultas);
+    return resp;
+
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+async function getAllTransfers() {
+  const consultas = {
+    text: 'SELECT t.fecha, c.name as origen, d.name as destino, t.monto FROM transfer t left join cliente c on t.id_cliente = c.id left join cliente d on t.id_destinatario = d.id',
+  };
+  try {
+    const resp = await pool.query(consultas);
+    return resp.rows;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+async function getAllUsers() {
+  const consultas = {
+    text: 'SELECT id, name, balance FROM cliente',
+  };
+  try {
+    const resp = await pool.query(consultas);
+    return resp.rows;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function newTransfer(data_transfer) {
+  const consultas = {
+    text: 'INSERT INTO transfer (id_cliente, id_destinatario, monto, comment) VALUES ($1, $2, $3, $4)',
+    values: Object.values(data_transfer),
+  };
+
+  const { id_cliente, id_destinatario, monto } = data_transfer;
+  const removeMoney = {
+    text: 'UPDATE cliente SET balance = balance - $2 WHERE id = $1',
+    values: [id_cliente, monto],
+  };
+
+  const addMoney = {
+    text: 'UPDATE cliente SET balance = balance + $2 WHERE id = $1',
+    values: [id_destinatario, monto],
+  };
+  try {
+    // return await pool.query(consultas);
+    await pool.query('BEGIN');
+    await pool.query(consultas);
+    await pool.query('COMMIT');
+    await pool.query(removeMoney);
+    await pool.query('COMMIT');
+    await pool.query(addMoney);
+    await pool.query('COMMIT');
+  } catch (error) {
+    await pool.query('ROLLBACK');
+    console.log(error.message);
+  }
+}
+
 //exportar modulos
-module.exports = {register_user,login}
+module.exports = {
+  register_user,
+  login,getAllTransfers,getAllUsers,newTransfer
+}
--- server.js
+++ server.js
@@ -6,19 +6,26 @@ const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const secret = '123';
 //traer modulos
-const {register_user,login} = require('./consultas.js');                                      
+const {
+  register_user,
+  login,getAllTransfers,getAllUsers,newTransfer
+} = require('./consultas.js');
 
 
 //levantar servidor
-const puerto = process.env.PUERTOS || 3000; 
-const servidor = process.env.HOSTORG || "localhost"; 
+const puerto = process.env.PUERTOS || 3000;
+const servidor = process.env.HOSTORG || "localhost";
 app.listen(puerto, () => console.log(`Servidor Disponible >>> http://${servidor}:${puerto} <<`));
 
 
 //usar expres de url y express de json para poder tomar el dato de parte del usuario
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({
+  extended: false
+}))
 //json
 app.use(express.json());
+// cookie parser
+app.use(cookieParser());
 //disponibilizar public
 
 
@@ -26,72 +33,119 @@ app.use(express.json());
 app.use("/bootstrap", express.static("/node_modules/bootstrap/dist"));
 
 //disponibilizar public y su contenido
-app.use("/js", express.static(__dirname + "/public/js"));//disponiblizando las carpetas de public y su contenido js
-app.use("/css", express.static(__dirname + "/public/css"));//disponiblizando las carpetas de public y su contenido css
+app.use("/js", express.static(__dirname + "/public/js")); //disponiblizando las carpetas de public y su contenido js
+app.use("/css", express.static(__dirname + "/public/css")); //disponiblizando las carpetas de public y su contenido css
 
 //configurar motor de vistas, dejandolo como handlebars
 app.set("view engine", "handlebars");
 //configurar motor de vistas
 app.engine(
-    "handlebars",
-    exphbs.engine({
-        defaultLayout: "main",
-        layoutsDir: `${__dirname}/views/layout`, //directorio de los layouts
-        partialsDir: `${__dirname}/views/partial`,//directorio de los parciales
-    })
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: `${__dirname}/views/layout`, //directorio de los layouts
+    partialsDir: `${__dirname}/views/partial`, //directorio de los parciales
+  })
 );
 //renderizar raiz
-app.get("/", (_req, res) => {
-    try {
-        res.render("login");
-    } catch (e) {
-        res.status(500).send({
-            error: `Algoo salió mal... ${e}`,
-            code: 500
-        })
-    };
-});
-app.get('/register', (_req, res) => {
-    res.render('register');
-});
-app.post('/login', async (req, res) => {
-    const user_data = req.body;
-  
-    try {
-      let resp = await login(user_data);
-     
-      if (resp.rowCount == 0) {
-        res.redirect('/');
+app.get("/", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res.render('login');
       } else {
-        const current_user = resp.rows[0];
-        const token = jwt.sign(current_user, secret);
-        
-        res.cookie('token', token);//utilizamos los cookies para poder almacenar el token // tambien se usa localStorage
         res.redirect('/dashboard');
       }
-    } catch (error) {
-      res.status(402).send({
-        code: 402,
-        message: error.message,
-      });
-    }
-  });
-app.post('/register', async (req, res) => {
-    const user_data = req.body;
-   
-    try {
-        await register_user(user_data);
-        res.redirect('/dashboard');
-    } catch (error) {
-        res.status(500).send({
-            code: 500,
-            message: 'No se pudo crear Usuario',
-          });
-    }
-})
-app.get('/dashboard', (_req, res) => {
-    res.render('dashboard');
+    });
+  } else {
+    res.render('login');
+  }
 });
-app.get('/logout', (_req, res) => {
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+// login verificando con jwt 
+app.post('/login', async (req, res) => {
+  const user_data = req.body;
+
+  try {
+    let resp = await login(user_data);
+
+    if (resp.rowCount == 0) {
+      res.redirect('/');
+    } else {
+      const current_user = resp.rows[0];
+      const token = jwt.sign(current_user, secret);
+
+      res.cookie('token', token); //utilizamos los cookies para setear el token 
+      // tambien se usa localStorage pero no es recomendable porque estamos en el back,
+      // localStorage se usa en el front
+      res.redirect('/dashboard');
+    }
+  } catch (error) {
+    res.status(402).send({
+      code: 402,
+      message: error.message,
+    });
+  }
+});
+// registrar nuevo usuario
+app.post('/register', async (req, res) => {
+  const user_data = req.body;
+
+  try {
+    await register_user(user_data);
+    res.redirect('/dashboard');
+  } catch (error) {
+    res.status(500).send({
+      code: 500,
+      message: 'No se pudo crear Usuario',
+    });
+  }
+})
+app.get('/dashboard', async (req, res) => {
+  const {
+    token
+  } = req.cookies;
+  let users = await getAllUsers();
+  let saldo = 0;
+  const transfers = await getAllTransfers();
+  if (token) {
+    jwt.verify(token, secret, (err, decoded) => {
+      saldo = users.find((user) => user.id == decoded.id).balance;
+      users = users.filter((user) => user.id != decoded.id);
+      if (err) {
+        res.redirect('/');
+      } else {
+        res.render('dashboard', {
+          current_user: decoded,
+          users,
+          transfers,
+          saldo,
+        });
+      }
+    });
+  } else {
     res.redirect('/');
+  }
+});
+app.post('/transfer', async (req, res) => {
+  const data_transfer = req.body;
+  try {
+    await newTransfer(data_transfer);
+    res.redirect('/dashboard');
+  } catch (error) {
+    res.status(500).send({
+      code: 500,
+      message: error.message,
+    });
+  }
+});
+
+// logout(salida)
+app.get('/logout', (req, res) => {
+  res.clearCookie('token'); //se limpia el token al salir
+  res.redirect('/');
 });
